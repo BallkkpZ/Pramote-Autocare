ebb316a84a3c1eaa6593a0d651bf189f5fac6014
BallkkpZ <ballkhongkrapan@gmail.com>
2025-12-24 10:41:05 +0700

    Fix: Ensure Amplify is properly configured and imported

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
-
-// 1. เพิ่มบรรทัดนี้: นำเข้าเครื่องมือของ AWS
 import { Amplify } from 'aws-amplify';
 
-// 2. เพิ่มบรรทัดนี้: ตั้งค่าให้รู้จักกับ Cognito User Pool ของคุณ
 Amplify.configure({
   Auth: {
     Cognito: {
-      userPoolId: 'ap-southeast-1_8jJsk7kY9',  // รหัสบัตรประชาชนระบบ (User Pool ID)
-      userPoolClientId: '3r9v3t1t71p2vrbokia31856b8', // รหัสแอป (Client ID)
+      userPoolId: 'ap-southeast-1_8jJsk7kY9',
+      userPoolClientId: '3r9v3t1t71p2vrbokia31856b8',
       loginWith: {
         email: true,
       },
@@ -25,4 +22,8 @@ Amplify.configure({
   }
 });
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById("root");
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+}
