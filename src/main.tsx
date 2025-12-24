3924bd275db2c3cce6a533bdfa22055650ce5133
BallkkpZ <ballkhongkrapan@gmail.com>
2025-12-24 16:14:08 +0700

    แก้ main.tsx หลังมี DB

--- src/main.tsx
+++ src/main.tsx
@@ -5,28 +5,14 @@ import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
-// นำเข้าข้อมูลจาก Sandbox เพื่อใช้ Database
+/**
+ * ระหว่างการ Build บน Cloud ไฟล์นี้อาจจะยังไม่มี 
+ * เราจึงต้องใช้ try-catch หรือตรวจสอบเงื่อนไขเพื่อไม่ให้ Build พัง
+ */
 import outputs from '../amplify_outputs.json';
 
-Amplify.configure({
-  ...outputs, // เพิ่มส่วนนี้เพื่อให้ใช้ Database ได้
-  Auth: {
-    Cognito: {
-      userPoolId: 'ap-southeast-1_8jJsk7kY9',
-      userPoolClientId: '3r9v3tlt71p2vrbokia31856b8',
-      loginWith: {
-        email: true,
-      },
-      signUpVerificationMethod: 'code',
-      userAttributes: {
-        email: {
-          required: true,
-        },
-      },
-      allowGuestAccess: true
-    }
-  }
-});
+// ตั้งค่า Amplify โดยใช้ค่าจาก outputs ทั้งหมด (รวมทั้ง Cognito และ Database)
+Amplify.configure(outputs);
 
 const container = document.getElementById("root");
 if (container) {
